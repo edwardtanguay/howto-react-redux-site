12761d426a8118a595a0dca56e07a0c065174046
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-07 13:14:03 +0200

    works

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.scss'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './features/counter/counterSlice';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-		<h1>Info Site</h1>
-		<p>Welcome to this site.</p>
-    </div>
-  )
+	const count = useSelector((state) => state.counter.value);
+	const dispatch = useDispatch();
+
+	return (
+		<div className="App">
+			<h1>Info Site</h1>
+			<button onClick={() => dispatch(increment())}>Increment</button>
+			<button onClick={() => dispatch(decrement())}>Decrement</button>
+			<hr />
+			<div>Count: {count}</div>
+		</div>
+	);
 }
 
 export default App;
